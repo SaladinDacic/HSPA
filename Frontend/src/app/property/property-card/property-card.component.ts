a30aed52ddc2dcf667c7924ea96315878da64c30
Saladin Dacić <you@example.comHajat1995@protonmail.com>
2020-08-15 20:36:51 +0200

    Start git for all files in HSPA

---
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component(
+  {
+    selector: 'app-property-card',
+    // template: `<h1> The template of card object </h1>`,
+    templateUrl: 'property-card.component.html',
+    // styles: ['h1 {font-weight: normal;}']
+    styleUrls: ['property-card.component.css']
+
+  }
+)
+export class PropertyCardComponent{
+  Property: any = {
+    "Id": 1,
+    "Name": "Birla House",
+    "Type": "House",
+    "Price": 12000
+  }
+
+}

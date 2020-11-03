cb35408ab624081d784eff9c08add299fa48210f
Saladin Dacić <you@example.comHajat1995@protonmail.com>
2020-11-03 20:25:37 +0100

    početak rada u api, kreiranje city kontrolera, osnove Program.cs i Startup.cs i launchSettings.json (rad backend-a kao api)

---
+++ WebAPI/Controllers/CityController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+//using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        public CityController()
+        {
+        }
+
+        [HttpGet("")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] {"Atlanta", "New York", "Sarajevo" };
+        }
+
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "Atlanta";
+        }
+
+    }
+}
